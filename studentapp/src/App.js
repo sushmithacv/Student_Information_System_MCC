d5de40699ab132ad87a9a69e5ed5d34f8b78fe2d
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-11-21 22:31:27 +0530

    Student Table Layout & Styles done

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       
-      <h1>Student Information Portal</h1> 
+      <h1>Student Information Portal </h1> 
 
       <hr></hr>
 
--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -1,7 +1,21 @@
+import '../assests/css/compo.css'
+
 export default function StudentTable(props){
     return(
 
-        <div>
+        <div classname=".outerDiv">
+            <div classname=".leftDiv">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>FirstName</td>
+                            <td>LastName</td>
+                            <td>Course</td>
+                            <td>Country</td>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     )
         
